6f8edf635a461033b2f5f96d5122354e52d710a0
Réda Maizate <reda.maizate@affluences.com>
2022-05-25 01:13:57 +0200

    Removed Kinesis and now passed through S3

--- mobile_app/terraform/main.tf
+++ mobile_app/terraform/main.tf
@@ -12,8 +12,19 @@ provider "aws" {
   profile = "redaprofile"
 }
 
-resource "aws_kinesis_video_stream" "kinesis_video_stream" {
-  name                    = "reda-test-video-stream"
-  data_retention_in_hours = 1
-  media_type              = "video/h264"
+#resource "aws_kinesis_video_stream" "kinesis_video_stream" {
+#  name                    = "reda-test-video-stream"
+#  data_retention_in_hours = 1
+#  media_type              = "video/h264"
+#}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "safety-drive-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_policy" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  block_public_acls   = false
+  block_public_policy = false
 }
