2ece060e27b626c62951e15d047742c195b89481
Réda Maizate <reda.maizate@affluences.com>
2022-06-21 00:25:18 +0200

    Applied black and pylint to serverless files

--- infrastructure/script_create_db.py
+++ infrastructure/script_create_db.py
@@ -4,7 +4,7 @@ import pymysql
 
 def run():
     if len(sys.argv) == 4:
-        endpoint = sys.argv[1].split(':')[0]
+        endpoint = sys.argv[1].split(":")[0]
         username = sys.argv[2]
         password = sys.argv[3]
 
@@ -13,24 +13,26 @@ def run():
             host=endpoint,  # Point de terminaison
             user=username,
             password=password,
-            port=3306
+            port=3306,
         )
         cursor = db.cursor()
 
-        sql = '''create database db_safety_drive'''
+        sql = """create database db_safety_drive"""
         cursor.execute(sql)
 
         cursor.connection.commit()
-        sql = '''use db_safety_drive'''
+        sql = """use db_safety_drive"""
         cursor.execute(sql)
 
-        sql = '''CREATE TABLE USERS (id_user integer primary key, email varchar(128) not null, password varchar(512) not null, pseudo varchar(128) not null)'''
+        sql = """CREATE TABLE USERS (id_user integer primary key, email varchar(128) not null, password varchar(512) not null, pseudo varchar(128) not null)"""
         cursor.execute(sql)
-        sql = '''CREATE TABLE PREDICTIONS (id_prediction integer primary key, value integer not null, datetime datetime not null, user integer references USERS(id_user))'''
+        sql = """CREATE TABLE PREDICTIONS (id_prediction integer primary key, value integer not null, datetime datetime not null, user integer references USERS(id_user))"""
         cursor.execute(sql)
     else:
-        print(f"Argument(s) missing, try : \"python script_create_db.py (endpoint) (user_db) (password_db)\"")
+        print(
+            f'Argument(s) missing, try : "python script_create_db.py (endpoint) (user_db) (password_db)"'
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
--- infrastructure/src/serverless/lambda_serverless.py
+++ infrastructure/src/serverless/lambda_serverless.py
@@ -4,9 +4,9 @@ import os
 
 
 def lambda_handler(event, context):
-    endpoint = os.environ.get('ENDPOINT').split(':')[0]
-    username = os.environ.get('MASTER_USERNAME')
-    password = os.environ.get('MASTER_PASSWORD')
+    endpoint = os.environ.get("ENDPOINT").split(":")[0]
+    username = os.environ.get("MASTER_USERNAME")
+    password = os.environ.get("MASTER_PASSWORD")
 
     # print("Globals", endpoint)
     # print("Globals", username)
@@ -17,18 +17,15 @@ def lambda_handler(event, context):
         host=endpoint,  # Point de terminaison
         user=username,
         password=password,
-        port=3306
+        port=3306,
     )
     cursor = db.cursor()
-    sql = '''use db_safety_drive'''
+    sql = """use db_safety_drive"""
     cursor.execute(sql)
-    cursor.execute('SELECT * FROM USERS')
+    cursor.execute("SELECT * FROM USERS")
     rows = cursor.fetchall()
 
     for row in rows:
         print(row)
 
-    return {
-        'statusCode': 200,
-        'body': "OK"
-    }
+    return {"statusCode": 200, "body": "OK"}
