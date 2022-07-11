5c758fbeec56e9655e9e7b951802cb3e7f2bee6a
La Kartoffel <56917344+CedricGARVENES@users.noreply.github.com>
2022-07-11 19:11:32 +0200

    serverless aurora-mysql done

--- infrastructure/terraform/modules/data_storage/outputs.tf
+++ infrastructure/terraform/modules/data_storage/outputs.tf
@@ -3,13 +3,13 @@ output "s3_bucket_id" {
 }
 
 output "db_endpoint" {
-  value = aws_db_instance.DB.endpoint
+  value = aws_rds_cluster_instance.rds-instance.endpoint
 }
 
 output "db_username" {
-  value = aws_db_instance.DB.username
+  value = aws_rds_cluster.rds-cluster.master_username
 }
 
 output "db_password" {
-  value = aws_db_instance.DB.password
+  value = aws_rds_cluster.rds-cluster.master_password
 }
