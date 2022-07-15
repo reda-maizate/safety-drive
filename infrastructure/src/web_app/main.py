424817dc31c4be480797df66407b575213bdd66d
Réda Maizate <reda.maizate@affluences.com>
2022-07-15 16:29:25 +0200

    Web app serverless

--- infrastructure/src/web_app/main.py
+++ infrastructure/src/web_app/main.py
@@ -1,5 +1,18 @@
+import json
+import pymysql
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from mangum import Mangum
+import os
+import logging
+
+
+endpoint = os.environ.get("ENDPOINT").split(":")[0]
+username = os.environ.get("MASTER_USERNAME")
+password = os.environ.get("MASTER_PASSWORD")
+db_name = "db_safety_drive"
+
+LOGGER = logging.getLogger(__name__)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -7,10 +20,25 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    try:
+        conn = pymysql.connect(
+            host=endpoint,
+            user=username,
+            password=password,
+            db=db_name,
+            connect_timeout=5,
+        )
+    except pymysql.MySQLError as e:
+        LOGGER.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+        LOGGER.error(e)
+
+    with conn.cursor() as session:
+        session.execute("SELECT * FROM users")
+        result = session.fetchall()
+        LOGGER.info(result)
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "users": json.dumps(result)}
+    )
 
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+handler = Mangum(app)
