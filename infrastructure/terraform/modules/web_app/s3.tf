e8c875bb01613665ce45ce37f37c975e8f5125bc
Réda Maizate <reda.maizate@affluences.com>
2022-07-15 17:07:26 +0200

    archive the website

--- infrastructure/terraform/modules/web_app/s3.tf
+++ infrastructure/terraform/modules/web_app/s3.tf
@@ -1,17 +1,36 @@
-data "archive_file" "website" {
-  type = "zip"
-  source_dir = "../../../src/web_app"
-  output_path = "../../../src/web_app.zip"
+#data "archive_file" "website" {
+#  type = "zip"
+#  source_dir = "../../../src/web_app"
+#  output_path = "../../../src/web_app.zip"
+#}
+
+resource "null_resource" "zip_app_and_dependencies" {
+  triggers = {
+    python_file = md5(file("../../../src/web_app/main.py"))
+    html_file   = md5(file("../../../src/web_app/templates/index.html"))
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      cd ../../../src/web_app
+      pip install --target ./package -r ../../requirements-web-app.txt
+      cd package
+      zip -r ../web_app.zip .
+      cd ..
+      zip -g web_app.zip templates
+      zip -g web_app.zip main.py
+    EOF
+  }
 }
 
 
 resource "aws_s3_object" "website" {
   depends_on = [
-    data.archive_file.website
+    null_resource.zip_app_and_dependencies
   ]
   bucket = var.s3_bucket_name
   key    = "website.zip"
-  source = data.archive_file.website.output_path
+  source = "../../../src/web_app.zip"
 
-  etag = filemd5(data.archive_file.website.output_path)
+  etag = filemd5("../../../src/web_app.zip")
 }
