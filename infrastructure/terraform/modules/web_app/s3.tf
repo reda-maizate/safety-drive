b2787fea918ed424f348f434479cb2e3eb449643
Réda Maizate <reda.maizate@affluences.com>
2022-07-16 11:48:58 +0200

    Zip the web app v2

--- infrastructure/terraform/modules/web_app/s3.tf
+++ infrastructure/terraform/modules/web_app/s3.tf
@@ -26,7 +26,7 @@ resource "aws_s3_object" "website" {
 #  ]
   bucket = var.s3_bucket_name
   key    = "website.zip"
-  source = "../../../src/web_app.zip"
+  source = "../../../src/web_app/web_app.zip"
 
-  etag = filemd5("../../../src/web_app.zip")
+  etag = filemd5("../../../src/web_app/web_app.zip")
 }
