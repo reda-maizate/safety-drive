5815853098066f3140b2ae2d45f3dd150842800d
Réda Maizate <reda.maizate@affluences.com>
2022-07-16 13:26:14 +0200

    Added index.html

--- infrastructure/src/zip_and_package.sh
+++ infrastructure/src/zip_and_package.sh
@@ -3,7 +3,7 @@ pip install --target ./package -r ../../requirements-web-app.txt
 cd package
 zip -r ../web_app.zip .
 cd ..
-zip -g web_app.zip templates
+zip -g web_app.zip templates/index.html
 zip -g web_app.zip main.py
 zip -g web_app.zip __init__.py
 rm -rf package
