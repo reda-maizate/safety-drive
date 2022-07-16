ccaaa293bc765ea0e278c31716916e5f387e6447
Réda Maizate <reda.maizate@affluences.com>
2022-07-16 11:44:04 +0200

    Zip the web app

--- infrastructure/terraform/modules/web_app/s3.tf
+++ infrastructure/terraform/modules/web_app/s3.tf
@@ -4,25 +4,26 @@
 #  output_path = "../../../src/web_app.zip"
 #}
 
-resource "null_resource" "zip_app_and_dependencies" {
-  provisioner "local-exec" {
-    command = <<EOF
-      cd ../../../src/web_app
-      pip install --target ./package -r ../../requirements-web-app.txt
-      cd package
-      zip -r ../web_app.zip .
-      cd ..
-      zip -g web_app.zip templates
-      zip -g web_app.zip main.py
-    EOF
-  }
-}
+#resource "null_resource" "zip_app_and_dependencies" {
+#  provisioner "local-exec" {
+#    command = <<EOF
+#      cd ../../../src/web_app
+#      pip install --target ./package -r ../../requirements-web-app.txt
+#      cd package
+#      zip -r ../web_app.zip .
+#      cd ..
+#      zip -g web_app.zip templates
+#      zip -g web_app.zip main.py
+#      rm -rf package
+#    EOF
+#  }
+#}
 
 
 resource "aws_s3_object" "website" {
-  depends_on = [
-    null_resource.zip_app_and_dependencies
-  ]
+#  depends_on = [
+#    null_resource.zip_app_and_dependencies
+#  ]
   bucket = var.s3_bucket_name
   key    = "website.zip"
   source = "../../../src/web_app.zip"
