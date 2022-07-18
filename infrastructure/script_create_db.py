0325b7346ab32982c7ad042fc7f64305b6100b85
La Kartoffel <56917344+CedricGARVENES@users.noreply.github.com>
2022-07-18 23:54:15 +0200

    first commit flutter

--- infrastructure/script_create_db.py
+++ infrastructure/script_create_db.py
@@ -17,16 +17,16 @@ def run():
         )
         cursor = db.cursor()
 
-        sql = """create database db_safety_drive"""
-        cursor.execute(sql)
+        # sql = """create database safety_drive_db"""
+        # cursor.execute(sql)
 
         cursor.connection.commit()
-        sql = """use db_safety_drive"""
+        sql = """use safety_drive_db"""
         cursor.execute(sql)
 
-        sql = """CREATE TABLE USERS (id_user integer primary key auto_increment, email varchar(128) not null, password varchar(512) not null, pseudo varchar(128) not null)"""
+        sql = """CREATE TABLE USERS (id_user varchar(512)  primary key, email varchar(128) not null, password varchar(512) not null, pseudo varchar(128) not null)"""
         cursor.execute(sql)
-        sql = """CREATE TABLE PREDICTIONS (id_prediction integer primary key auto_increment, value integer not null, datetime datetime not null, user integer references USERS(id_user))"""
+        sql = """CREATE TABLE PREDICTIONS (id_prediction integer primary key, value integer not null, datetime datetime not null, user integer references USERS(id_user))"""
         cursor.execute(sql)
     else:
         print(
